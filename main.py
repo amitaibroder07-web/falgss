19a4bb63ac13f4c4cee9731d212dd7e4f7b30688
jbt <itaychalfon@gmail.com>
2026-08-13 10:42:01 +0300

    initial commmit2

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame
+import consts
+
+state= {
+        "is_window_open": True,
+        "is_on_boom": False,
+        ""
+
+
+
+        }
+
+
+def handle_user_events():
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            state["is_window_open"] = False
+
+
+        elif state["state"] != consts.RUNNING_STATE:
+            continue
+
